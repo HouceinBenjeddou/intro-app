fd1edc963e3f4d7e9864f277767fb6b3f4a2abe2
HouceinBenjeddou <hou52cine@gmail.com>
2022-12-29 22:19:05 +0100

    init

---
+++ src/app/intro.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'hello-world',
+    template: `
+    <h2>Hello world</h2>
+    <p>this is my first Component</p>
+    `
+})
+
+export class intro{}
